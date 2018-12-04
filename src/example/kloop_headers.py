fcde3464c5bb9fd701985a9e2f027d0cacd3fda5
aydarbekov <email@example.com>
2018-12-05 00:59:49 +0600

    d

--- src/example/kloop_headers.py
+++ src/example/kloop_headers.py
@@ -9,7 +9,7 @@ def extractdata(context, data):
 
     header = _gettext(page.xpath('.//header/h1/text()'))
     autor = _gettext(page.xpath('.//header/div/div/a/text()'))
-    text = _gettext(page.xpath('./html/body/div[6]/div[2]/div'))
+    text = _gettext(page.xpath('.//article/text()'))
 	
     article_data = {
         "url": response.url,
