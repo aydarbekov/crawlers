5b7c17cb51d1f9bbfe9621fe6da346af4055c655
aydarbekov <email@example.com>
2018-12-04 20:41:26 +0600

    new5

--- src/example/kloop_headers.py
+++ src/example/kloop_headers.py
@@ -8,7 +8,7 @@ def extractdata(context, data):
     # Parse the rest of the page to extract structured data.
 
     header = _gettext(page.xpath('.//header/h1/text()'))
-	autor = _gettext(page.xpath('.//header/div/div/a/text()'))
+    autor = _gettext(page.xpath('.//header/div/div/a/text()'))
 	
     article_data = {
         "url": response.url,
