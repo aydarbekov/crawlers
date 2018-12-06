519d888e6864bbf1ba3933e8256bd2e4abe7bb80
aydarbekov <email@example.com>
2018-12-06 17:36:05 +0600

    adress_grs

---
+++ src/example/adres_grs.py
@@ -0,0 +1,27 @@
+def extractdata(context, data):
+    # This stage comes after 'fetch' so the 'data' input contains an
+    # HTTPResponse object.
+    response = context.http.rehash(data)
+    url = response.url
+    page = response.html
+
+    # Parse the rest of the page to extract structured data.
+
+    name = _gettext(page.xpath("//div[@class='list-street']/p"))
+    
+	
+    org_data = {
+	    "url": response.url,
+	    "name": name
+	    
+    }
+    print("----------------PRINTING ORG_DATA----------------")
+    print(org_data)
+	
+    context.emit(data=org_data)
+
+def _gettext(list):
+    if not list:
+        return list
+    else:
+        return list[0].strip()
