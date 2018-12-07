12701130fec1058fbde75ac182ff1173ec6811c9
aydarbekov <email@example.com>
2018-12-08 00:47:56 +0600

    a

--- src/example/adres_grs.py
+++ src/example/adres_grs.py
@@ -10,8 +10,8 @@ def extractdata(context, data):
     
     
     for i in range(len(page.xpath('//tbody/tr'))):
-        id_street = _gettext(page.xpath("//tbody/tr['+str(i)+']/td[2]/text()"))
-        street_kg = _gettext(page.xpath("//tbody/tr['+str(i)+']/td[3]/div/p/text()"))
+        id_street = _gettext(page.xpath('//tbody/tr['+str(i)+']/td[2]/text()'))
+        street_kg = _gettext(page.xpath('//tbody/tr['+str(i)+']/td[3]/div/p/text()'))
         
         org_data = {
             "id_street": id_street,
