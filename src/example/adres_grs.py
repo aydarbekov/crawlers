ef6b9c4eac54ce28e1b4788da180c5833267476a
aydarbekov <email@example.com>
2018-12-08 01:23:11 +0600

    a

--- src/example/adres_grs.py
+++ src/example/adres_grs.py
@@ -14,13 +14,14 @@ def extractdata(context, data):
         i=i+1
         id_street = (_gettext(page.xpath('//tbody/tr['+str(i)+']/td[2]/text()')))
         street_kg = (_gettext(page.xpath('//tbody/tr['+str(i)+']/td[3]/div/p/text()')))
-        print("----------------PRINTING ORG_DATA----------------")
-        print(org_data)
+        
         
         org_data = {
             "id_street": id_street,
             "street_kg": street_kg
             }
+        print("----------------PRINTING ORG_DATA----------------")
+        print(org_data)
         id_street=[]
         street_kg=[]
         
