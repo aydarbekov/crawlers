2ce7dd237835c74f0b4647dcd6e9b48dd949579a
aydarbekov <email@example.com>
2018-12-07 15:10:35 +0600

    grs replace tree

--- src/example/adres_grs.py
+++ src/example/adres_grs.py
@@ -7,7 +7,7 @@ def extractdata(context, data):
 
     # Parse the rest of the page to extract structured data.
 
-    street_kg = _gettext(tree.xpath('//tbody/tr['+str(j)+']/td[3]/div/p/text()'))
+    street_kg = _gettext(page.xpath('//tbody/tr['+str(j)+']/td[3]/div/p/text()'))
     
     org_data = {
         "street_kg": street_kg,
@@ -17,7 +17,7 @@ def extractdata(context, data):
     for i in range(len(rows)):
         j = i+1
         result = {}
-        street_kg = _gettext(tree.xpath('//tbody/tr['+str(j)+']/td[3]/div/p/text()'))
+        street_kg = _gettext(page.xpath('//tbody/tr['+str(j)+']/td[3]/div/p/text()'))
         result['street_kg'] = street_kg
         print (result)
     context.emit(data=org_data)
