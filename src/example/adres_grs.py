848a05d3f836078d85ec455a407f4b2e5b72675b
aydarbekov <email@example.com>
2018-12-07 20:26:46 +0600

    dddd

--- src/example/adres_grs.py
+++ src/example/adres_grs.py
@@ -19,9 +19,9 @@ def extractdata(context, data):
         }
         print("----------------PRINTING ORG_DATA----------------")
         print(org_data)
-        
-        context.emit(data=org_data)
     return org_data
+        context.emit(data=org_data)
+    
    
 def clean_dict(data):
     result = {}
